5f04dbf7d6083a0e2e13f1650e2b2faaff2a65a0
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-03-26 15:36:23 -0300

    :sparkles: feat: Profile Dinâmico

--- src/pages/Blog/Components/Profile/index.tsx
+++ src/pages/Blog/Components/Profile/index.tsx
@@ -1,3 +1,6 @@
+import { useState, useEffect, useCallback } from 'react'
+
+import { api } from '../../../../lib/axios'
 import { ExternalLink } from '../../../../Components/ExternalLink'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -5,36 +8,66 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faBuilding, faUserGroup } from '@fortawesome/free-solid-svg-icons'
 import { ProfileContainer, ProfileDetails, ProfilePicture } from './styles'
 
+const username = import.meta.env.VITE_GITHUB_USERNAME
+
+interface ProfileData {
+  login: string
+  bio: string
+  avatar_url: string
+  html_url: string
+  name: string
+  company?: string
+  followers: number
+}
+
 export function Profile() {
+  const [profileData, setProfileData] = useState<ProfileData>({} as ProfileData)
+
+  const getProfileData = useCallback(async () => {
+    try {
+      const response = await api.get(`/users/${username}`)
+
+      setProfileData(response.data)
+    } finally {
+      /* empty */
+    }
+  }, [])
+
+  useEffect(() => {
+    getProfileData()
+  }, [getProfileData])
+
   return (
     <ProfileContainer>
-      <ProfilePicture src="https://github.com/WD-GabrielKenji.png" />
+      <ProfilePicture src={profileData.avatar_url} />
 
       <ProfileDetails>
         <header>
-          <h1>Gabriel Kenji</h1>
+          <h1>{profileData.name}</h1>
 
-          <ExternalLink text="Github" href="#" />
+          <ExternalLink
+            text="Github"
+            href={profileData.html_url}
+            target="_blank"
+          />
         </header>
 
-        <p>
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
-          pulvinar vel mass.
-        </p>
+        <p>{profileData.bio}</p>
 
         <ul>
           <li>
             <FontAwesomeIcon icon={faGithub} />
-            WD-GabrielKenji
+            {profileData.login}
           </li>
-          <li>
-            <FontAwesomeIcon icon={faBuilding} />
-            Rocketseat
-          </li>
+          {profileData?.company && (
+            <li>
+              <FontAwesomeIcon icon={faBuilding} />
+              {profileData.company}
+            </li>
+          )}
           <li>
             <FontAwesomeIcon icon={faUserGroup} />
-            32 seguidores
+            {profileData.followers} seguidores
           </li>
         </ul>
       </ProfileDetails>
