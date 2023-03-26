74b4c6c4d3a8304028242c1d53c4733e4ef59114
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-03-26 13:08:36 -0300

    :sparkles: feat: Acessando API do Github

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,10 +1,48 @@
+import { useCallback, useEffect, useState } from 'react'
+
+import { api } from '../../lib/axios'
 import { Post } from './Components/Post'
 import { Profile } from './Components/Profile'
 import { SearchForm } from './Components/SearchForm'
 
 import { PostsListContainer } from './styles'
 
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const repoName = import.meta.env.VITE_GITHUB_REPONAME
+
+export interface IPost {
+  title: string
+  body: string
+  created_at: string
+  number: number
+  html_url: string
+  comments: number
+  user: {
+    login: string
+  }
+}
+
 export function Blog() {
+  const [posts, setPosts] = useState<IPost[]>([])
+
+  const getPosts = useCallback(
+    async (query: string = '') => {
+      try {
+        const response = await api.get(
+          `/search/issues?q=${query}%20repo:${username}/${repoName}`,
+        )
+        console.log(response.data)
+        setPosts(response.data.items)
+      } finally {
+      }
+    },
+    [posts],
+  )
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
     <>
       <Profile />
