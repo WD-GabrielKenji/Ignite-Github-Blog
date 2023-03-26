ee47ee778c33864720c2a3aae78d179ffc96b2c6
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-03-26 15:24:19 -0300

    :sparkles: feat: Buscando Post

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -46,7 +46,7 @@ export function Blog() {
   return (
     <>
       <Profile />
-      <SearchForm />
+      <SearchForm postsLength={posts.length} getPosts={getPosts} />
 
       <PostsListContainer>
         {posts.map((post) => (
