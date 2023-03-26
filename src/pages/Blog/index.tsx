f464e76068b6e76062d74a9f7a3d7fe04b315a06
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-03-25 21:14:56 -0300

    :sparkles: feat: Componente Post (estático)

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,23 @@
+import { Post } from './Components/Post'
 import { Profile } from './Components/Profile'
 import { SearchForm } from './Components/SearchForm'
+
+import { PostsListContainer } from './styles'
 
 export function Blog() {
   return (
     <>
       <Profile />
       <SearchForm />
+
+      <PostsListContainer>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </PostsListContainer>
     </>
   )
 }
---
+++ src/pages/Blog/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const PostsListContainer = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(26rem, 1fr));
+  width: 100%;
+  margin-bottom: 14rem;
+  gap: 2rem;
+`
